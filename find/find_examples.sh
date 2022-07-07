13be2af16c805d123fc4a38f075eed02974fd298
rhairy <randall.harrison91@gmail.com>
2022-07-07 16:46:35 -0700

    Update find_examples.sh

--- find/find_examples.sh
+++ find/find_examples.sh
@@ -3,3 +3,7 @@
 # Get the total space used by log files on the system
 
 find / -name *.log -type f -exec du -ch {} + | sort -nr
+
+# Get the top 10 biggest files on the system
+
+find / -type f -exec du -ah {} + | sort -hr | head -n 10
